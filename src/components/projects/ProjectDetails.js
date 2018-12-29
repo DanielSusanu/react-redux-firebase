53051d8343390a48e44a4f9b8218cd566aefaf6e
Daniel Susanu <d.susanu.development@gmail.com>
2018-12-29 17:31:44 +0000

    Added all components

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ProjectDetails() {
+  return (
+    <div className="container section project-details">
+      <div className="card-z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project title</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectDetails
