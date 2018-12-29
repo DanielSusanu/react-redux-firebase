5fd3ae4519a812819db9d9728834125e5c8783f9
Daniel Susanu <d.susanu.development@gmail.com>
2018-12-29 18:16:32 +0000

    Added content,style and route links to components

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
-function ProjectDetails() {
+const ProjectDetails = (props) => {
+  console.log(props);
+  const id = props.match.params.id;
   return (
     <div className="container section project-details">
-      <div className="card-z-depth-0">
+      <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project title</span>
+          <span className="card-title">Project title - {id}</span>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </div>
+        <div className="card-action gret lighten-4 grey-text">
+          <div>Posted by Daniel</div>
+          <div>12/29/2018</div>
         </div>
       </div>
     </div>
